837347b828ef60532520dabdca6750c1e03f24d0
SebastianG302 <johan.guerrero@pi.edu.co>
2022-11-26 13:47:25 -0500

    reactive pages started

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre     : new FormControl('RTX 4080ti '),
+  //   precio     : new FormControl(1500),
+  //   existencias: new FormControl(3),
+  // })
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  campoNoEsValido(campo: string){
+    return this.miFormulario.controls[campo].errors
+        && this.miFormulario.controls[campo].touched;
+  }
 
   ngOnInit(): void {
   }
